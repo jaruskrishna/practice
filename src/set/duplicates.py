608a77a3dd214d16bb4b06968f6d5c5f93a1b2ba
Suraj K Kokkirala <45082276+jaruskrishna@users.noreply.github.com>
2022-09-17 14:40:58 -0700

    Practicing for sets & duplicate removal

---
+++ src/set/duplicates.py
@@ -0,0 +1,24 @@
+# Ways to remove duplicates from list
+
+def rem_dup(l):
+    print("\nThe original list" , l)
+    res = [*set(l)]
+
+    print("\nAfer removinf duplicates", res)
+
+
+def rem_dup_use_for(l):
+    print("\nThe original list", (l))
+
+    res = []
+    [res.append(x) for x in l if x not in res]
+
+    print("\nAfter removing the duplicates -", res)
+
+
+if __name__ ==  '__main__':
+    list1 = [21,22,1,1,3,5,4,21,22,334,4,6]
+    dict = {1 : 'one', 2 : 'two', 3:'three'}
+    set1 = {11,2,3,5,6,4,4}
+    #rem_dup(list1)
+    rem_dup_use_for(list1)
